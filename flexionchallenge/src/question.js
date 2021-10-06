b07ce04d94732a96da1a0c3fd7646eb6c45c6f55
Justin Adams <khisanth05@gmail.com>
2021-10-05 20:39:38 -0400

    Major - Completed base model of working tool. Teacher can check 5 quesetions in real time. Math rounds to 2 places, grades each answer as correct/incorrect

--- flexionchallenge/src/question.js
+++ flexionchallenge/src/question.js
@@ -1,9 +1,6 @@
 import "./App.css";
 import Container from "react-bootstrap/Container";
-import Button from "react-bootstrap/Button";
 import Table from "react-bootstrap/Table";
-// import Row from "react-bootstrap/Row";
-// import Col from "react-bootstrap/Col";
 import { useState, useEffect } from "react";
 
 var convert = require("convert-units");
@@ -12,40 +9,40 @@ function Test(props) {
   const [question1a, setQuestion1a] = useState("0");
   const [question1b, setQuestion1b] = useState("F");
   const [question1c, setQuestion1c] = useState("R");
-  const [question1d, setQuestion1d] = useState();
+  const [question1d, setQuestion1d] = useState("");
   const [answer1, setAnswer1] = useState(1);
   const [result1, setResult1] = useState();
 
   const [question2a, setQuestion2a] = useState("0");
   const [question2b, setQuestion2b] = useState("F");
   const [question2c, setQuestion2c] = useState("R");
-  const [question2d, setQuestion2d] = useState();
+  const [question2d, setQuestion2d] = useState("");
   const [answer2, setAnswer2] = useState(1);
   const [result2, setResult2] = useState();
 
   const [question3a, setQuestion3a] = useState("0");
   const [question3b, setQuestion3b] = useState("F");
   const [question3c, setQuestion3c] = useState("R");
-  const [question3d, setQuestion3d] = useState();
+  const [question3d, setQuestion3d] = useState("");
   const [answer3, setAnswer3] = useState(1);
   const [result3, setResult3] = useState();
 
   const [question4a, setQuestion4a] = useState("0");
   const [question4b, setQuestion4b] = useState("F");
   const [question4c, setQuestion4c] = useState("R");
-  const [question4d, setQuestion4d] = useState();
+  const [question4d, setQuestion4d] = useState("");
   const [answer4, setAnswer4] = useState(1);
   const [result4, setResult4] = useState();
 
   const [question5a, setQuestion5a] = useState("0");
   const [question5b, setQuestion5b] = useState("F");
   const [question5c, setQuestion5c] = useState("R");
-  const [question5d, setQuestion5d] = useState();
+  const [question5d, setQuestion5d] = useState("");
   const [answer5, setAnswer5] = useState(1);
   const [result5, setResult5] = useState();
 
   const convertProblems = () => {
-    setAnswer1(convert(question1a).from(question1b).to(question1c));
+    setAnswer1((convert(question1a).from(question1b).to(question1c)).toFixed(2));
         if(answer1 === question1d){
             setResult1("Correct");
         }
@@ -53,25 +50,25 @@ function Test(props) {
             setResult1("Incorrect");
         console.log("question1d", question1d);
         console.log("answer1", answer1);
-    setAnswer2(convert(question2a).from(question2b).to(question2c));
+    setAnswer2(convert(question2a).from(question2b).to(question2c).toFixed(2));
     if(answer2 === question2d){
         setResult2("Correct");
     }
     else
         setResult2("Incorrect");
-    setAnswer3(convert(question3a).from(question3b).to(question3c));
+    setAnswer3(convert(question3a).from(question3b).to(question3c).toFixed(2));
     if(answer3 === question3d){
         setResult3("Correct");
     }
     else
         setResult3("Incorrect");
-    setAnswer4(convert(question4a).from(question4b).to(question4c));
+    setAnswer4(convert(question4a).from(question4b).to(question4c).toFixed(2));
     if(answer4 === question4d){
         setResult4("Correct");
     }
     else
         setResult4("Incorrect");
-    setAnswer5(convert(question5a).from(question5b).to(question5c));
+    setAnswer5(convert(question5a).from(question5b).to(question5c).toFixed(2));
     if(answer5 === question5d){
         setResult5("Correct");
     }
@@ -80,27 +77,11 @@ function Test(props) {
   };
 
   useEffect(() =>{
-      setQuestion1d();
-      setQuestion2d();
-      setQuestion3d();
-      setQuestion4d();
-      setQuestion5d();
-  }, [])
+      convertProblems();
+  })
 
   return (
     <Container>
-      <datalist id="unitOfMeasurement">
-        <option>K</option>
-        <option>C</option>
-        <option>F</option>
-        <option>R</option>
-        <option>l</option>
-        <option>Tbs</option>
-        <option>in3</option>
-        <option>cup</option>
-        <option>ft3</option>
-        <option>gal</option>
-      </datalist>
 
       <Table striped bordered hover>
         <thead>
@@ -123,29 +104,51 @@ function Test(props) {
               ></input>
             </td>
             <td>
-              <input
-                type="text"
+              <select
                 list="unitOfMeasurement"
-                onChange={(e) => setQuestion1b(e.target.value)}
-              />
+                onChange={(e) => setQuestion1b(e.target.value)}>      
+                <option value=""></option>                  
+                <option value="K">Kelvin</option>
+                <option value="C">Celsius</option>
+                <option value="F">Fahrenheit</option>
+                <option value="R">Rankine</option>
+                <option value="l">Liter</option>
+                <option value="Tbs">Tablespoons</option>
+                <option value="in3">Cubic Inches</option>
+                <option value="cup">Cup</option>
+                <option value="ft3">Cubic Feet</option>
+                <option value="gal">Gallons</option>
+              </select>
+            </td>
+            <td>
+              <select
+                list="unitOfMeasurement"
+                onChange={(e) => setQuestion1c(e.target.value)}>     
+                <option value=""></option>                   
+                <option value="K">Kelvin</option>
+                <option value="C">Celsius</option>
+                <option value="F">Fahrenheit</option>
+                <option value="R">Rankine</option>
+                <option value="l">Liter</option>
+                <option value="Tbs">Tablespoons</option>
+                <option value="in3">Cubic Inches</option>
+                <option value="cup">Cup</option>
+                <option value="ft3">Cubic Feet</option>
+                <option value="gal">Gallons</option>
+              </select>
             </td>
             <td>
               <input
-                type="text"
-                list="unitOfMeasurement"
-                onChange={(e) => setQuestion1c(e.target.value)}
-              />
-            </td>
-            <td>
-              <input
-                type="textarea"
+                type="number"
                 onChange={(e) => setQuestion1d(e.target.value)}
               />
             </td>
-            <h4>{result1}</h4>
+            {question1d !== "" &&
+                <h4>{result1}</h4>
+            }
           </tr>
           <tr>
-            <td>2</td>
+            <td>1</td>
             <td>
               <input
                 type="textarea"
@@ -153,29 +156,47 @@ function Test(props) {
               ></input>
             </td>
             <td>
-              <input
-                type="text"
+              <select
                 list="unitOfMeasurement"
-                onChange={(e) => setQuestion2b(e.target.value)}
-              />
+                onChange={(e) => setQuestion2b(e.target.value)}>   
+                <option value=""></option>         
+                <option value="K">Kelvin</option>
+                <option value="C">Celsius</option>
+                <option value="F">Fahrenheit</option>
+                <option value="R">Rankine</option>
+                <option value="l">Liter</option>
+                <option value="Tbs">Tablespoons</option>
+                <option value="in3">Cubic Inches</option>
+                <option value="cup">Cup</option>
+                <option value="ft3">Cubic Feet</option>
+                <option value="gal">Gallons</option>
+              </select>
+            </td>
+            <td>
+              <select
+                list="unitOfMeasurement"
+                onChange={(e) => setQuestion2c(e.target.value)}>       
+                <option value=""></option>                 
+                <option value="K">Kelvin</option>
+                <option value="C">Celsius</option>
+                <option value="F">Fahrenheit</option>
+                <option value="R">Rankine</option>
+                <option value="l">Liter</option>
+                <option value="Tbs">Tablespoons</option>
+                <option value="in3">Cubic Inches</option>
+                <option value="cup">Cup</option>
+                <option value="ft3">Cubic Feet</option>
+                <option value="gal">Gallons</option>
+              </select>
             </td>
             <td>
               <input
-                type="text"
-                list="unitOfMeasurement"
-                onChange={(e) => setQuestion2c(e.target.value)}
+                type="number"
+                onChange={(e) => setQuestion2d(e.target.value)}
               />
             </td>
-            <td>              
-            <input
-                type="textarea"
-                onChange={(e) => setQuestion2d(e.target.value)}
-              ></input>
-            </td>
-            {
-            (question2d === answer2)
-              ? <h4>Correct</h4>
-              : <></>
+            {question2d !== "" &&
+                <h4>{result2}</h4>
             }
           </tr>
           <tr>
@@ -187,18 +208,38 @@ function Test(props) {
               ></input>
             </td>
             <td>
-              <input
-                type="text"
+              <select
                 list="unitOfMeasurement"
-                onChange={(e) => setQuestion3b(e.target.value)}
-              />
+                onChange={(e) => setQuestion3b(e.target.value)}>      
+                <option value=""></option>                  
+                <option value="K">Kelvin</option>
+                <option value="C">Celsius</option>
+                <option value="F">Fahrenheit</option>
+                <option value="R">Rankine</option>
+                <option value="l">Liter</option>
+                <option value="Tbs">Tablespoons</option>
+                <option value="in3">Cubic Inches</option>
+                <option value="cup">Cup</option>
+                <option value="ft3">Cubic Feet</option>
+                <option value="gal">Gallons</option>
+              </select>
             </td>
             <td>
-              <input
-                type="text"
+              <select
                 list="unitOfMeasurement"
-                onChange={(e) => setQuestion3c(e.target.value)}
-              />
+                onChange={(e) => setQuestion3c(e.target.value)}>     
+                <option value=""></option>                   
+                <option value="K">Kelvin</option>
+                <option value="C">Celsius</option>
+                <option value="F">Fahrenheit</option>
+                <option value="R">Rankine</option>
+                <option value="l">Liter</option>
+                <option value="Tbs">Tablespoons</option>
+                <option value="in3">Cubic Inches</option>
+                <option value="cup">Cup</option>
+                <option value="ft3">Cubic Feet</option>
+                <option value="gal">Gallons</option>
+              </select>
             </td>
             <td>
               
@@ -207,11 +248,9 @@ function Test(props) {
                 onChange={(e) => setQuestion3d(e.target.value)}
               ></input>
               </td>
-              {
-              ((question3d === answer3) && question3d !== "")
-                ? <h4>Correct</h4>
-                : <></>
-              }
+            {question3d !== "" &&
+                <h4>{result3}</h4>
+            }
             </tr>
           <tr>
             <td>4</td>
@@ -222,18 +261,38 @@ function Test(props) {
               ></input>
             </td>
             <td>
-              <input
-                type="text"
+              <select
                 list="unitOfMeasurement"
-                onChange={(e) => setQuestion4b(e.target.value)}
-              />
+                onChange={(e) => setQuestion4b(e.target.value)}>      
+                <option value=""></option>                  
+                <option value="K">Kelvin</option>
+                <option value="C">Celsius</option>
+                <option value="F">Fahrenheit</option>
+                <option value="R">Rankine</option>
+                <option value="l">Liter</option>
+                <option value="Tbs">Tablespoons</option>
+                <option value="in3">Cubic Inches</option>
+                <option value="cup">Cup</option>
+                <option value="ft3">Cubic Feet</option>
+                <option value="gal">Gallons</option>
+              </select>
             </td>
             <td>
-              <input
-                type="text"
+              <select
                 list="unitOfMeasurement"
-                onChange={(e) => setQuestion4c(e.target.value)}
-              />
+                onChange={(e) => setQuestion4c(e.target.value)}>     
+                <option value=""></option>                   
+                <option value="K">Kelvin</option>
+                <option value="C">Celsius</option>
+                <option value="F">Fahrenheit</option>
+                <option value="R">Rankine</option>
+                <option value="l">Liter</option>
+                <option value="Tbs">Tablespoons</option>
+                <option value="in3">Cubic Inches</option>
+                <option value="cup">Cup</option>
+                <option value="ft3">Cubic Feet</option>
+                <option value="gal">Gallons</option>
+              </select>
             </td>
             <td>
               
@@ -242,11 +301,9 @@ function Test(props) {
                 onChange={(e) => setQuestion4d(e.target.value)}
               ></input>
               </td>
-              {
-              (question4d === answer4)
-                ? <h4>Correct</h4>
-                : <></>
-              }
+            {question4d !== "" &&
+                <h4>{result4}</h4>
+            }
             </tr>
           <tr>
             <td>5</td>
@@ -257,18 +314,38 @@ function Test(props) {
               ></input>
             </td>
             <td>
-              <input
-                type="text"
+              <select
                 list="unitOfMeasurement"
-                onChange={(e) => setQuestion5b(e.target.value)}
-              />
+                onChange={(e) => setQuestion5b(e.target.value)}>    
+                <option value=""></option>                    
+                <option value="K">Kelvin</option>
+                <option value="C">Celsius</option>
+                <option value="F">Fahrenheit</option>
+                <option value="R">Rankine</option>
+                <option value="l">Liter</option>
+                <option value="Tbs">Tablespoons</option>
+                <option value="in3">Cubic Inches</option>
+                <option value="cup">Cup</option>
+                <option value="ft3">Cubic Feet</option>
+                <option value="gal">Gallons</option>
+              </select>
             </td>
             <td>
-              <input
-                type="text"
+              <select
                 list="unitOfMeasurement"
-                onChange={(e) => setQuestion5c(e.target.value)}
-              />
+                onChange={(e) => setQuestion5c(e.target.value)}>      
+                <option value=""></option>                  
+                <option value="K">Kelvin</option>
+                <option value="C">Celsius</option>
+                <option value="F">Fahrenheit</option>
+                <option value="R">Rankine</option>
+                <option value="l">Liter</option>
+                <option value="Tbs">Tablespoons</option>
+                <option value="in3">Cubic Inches</option>
+                <option value="cup">Cup</option>
+                <option value="ft3">Cubic Feet</option>
+                <option value="gal">Gallons</option>
+              </select>
             </td>
             <td>
               
@@ -277,15 +354,13 @@ function Test(props) {
                 onChange={(e) => setQuestion5d(e.target.value)}
               ></input>
               </td>
-              {
-              (question5d === answer5)
-                ? <h4>Correct</h4>
-                : <></>
-              }
+            {question5d !== "" &&
+                <h4>{result5}</h4>
+            }
             </tr>
         </tbody>
       </Table>
-      <button onClick={convertProblems}>Grade</button>
+      <button onClick={convertProblems}>Submit Test</button>
     </Container>
   );
 }
